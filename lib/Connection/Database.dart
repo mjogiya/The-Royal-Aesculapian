7f07ebc0e659c00953cfd52d69128c50d5bfe8e8
unknown <kakadiyam777@gmail.com>
2023-03-27 11:47:26 +0530

    added doctor querysnapshot

--- lib/Connection/Database.dart
+++ lib/Connection/Database.dart
@@ -1,13 +1,18 @@
-import'package:cloud_firestore/cloud_firestore.dart';
-import 'package:theroyalaesculapian/pages/Patient/Models/PatientsLog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:theroyalaesculapian/pages/Patient/Doctors.dart';
+import 'package:theroyalaesculapian/pages/Patient/Models/DoctorModel.dart';
 
-class DatabaseService  {
-
+class DatabaseService {
   final String uid;
-  DatabaseService({ required this.uid });
-  final CollectionReference Users = FirebaseFirestore.instance.collection('Users');
+  DatabaseService({required this.uid});
+  final CollectionReference Users =
+      FirebaseFirestore.instance.collection('Users');
   FirebaseFirestore db = FirebaseFirestore.instance;
-  Future RegisterUserData(String name, String email, String mobile, String password, String role) async {
+  final CollectionReference Doctors =
+      FirebaseFirestore.instance.collection('Doctor');
+
+  Future RegisterUserData(String name, String email, String mobile,
+      String password, String role) async {
     return await Users.doc(uid).set({
       'name': name,
       'email': email,
@@ -17,46 +22,53 @@ class DatabaseService  {
     });
   }
 
-    Future<String> getRoleData() async {
-      String role;
-      return await db.collection("Users").doc(uid).get().then((event) {
-          print(event.data()?['role']);
-      if(event.data()?['role'] == "Patient") {
+  Future<String> getRoleData() async {
+    String role;
+    return await db.collection("Users").doc(uid).get().then((event) {
+      print(event.data()?['role']);
+      if (event.data()?['role'] == "Patient") {
         role = "Patient";
       } else {
         role = "";
       }
       return role;
     });
-    }
-// //LIst of Patients
-// List<PatientsLog> _PatientsList(QuerySnapshot snapshot) {
-//     return snapshot.docs.map((doc){
-//       return PatientsLog(
+  }
+
+//LIst of Doctors
+// Stream<QuerySnapshot> get Doctors{
+
+// db.collection("cities").where("capital", isEqualTo: true).get().then(
+//       (querySnapshot) {
+//         print("Successfully completed");
+//         for (var docSnapshot in querySnapshot.docs) {
+//           print('${docSnapshot.id} => ${docSnapshot.data()}');
+//         }
+//       },
+//       onError: (e) => print("Error completing: $e"),
+//     );
+// }
+
+//Setting up the stream
+  Stream<QuerySnapshot> get Doctor {
+    return Doctors.snapshots();
+  }
+
+  Stream<QuerySnapshot> get User {
+    return Users.snapshots();
+  }
+
+// List<DoctorModel>? _DoctorList(QuerySnapshot snapshot){
+//   return snapshot.docs.map((doc) {
+//       //print(doc.data);
+//       return DoctorList(
 //           name: doc.data['name'] ?? '',
-//         email: doc.data['email'] ?? '',
-//         mobile: doc.data['mobile'] ?? ''
-//       );
+//           strength: doc.data['strength'] ?? 0,
+//           sugars: doc.data['sugars'] ?? '0');
 //     }).toList();
+//   }
 // }
 
 
-
-
-
-  // final docRef = collecti.collection("cities").doc("SF");
-  // docRef.get().then(
-  // (DocumentSnapshot doc) {
-  // final data = doc.data() as Map<String, dynamic>;
-  // // ...
-  // },
-  // onError: (e) => print("Error getting document: $e"),
-  // );
-
-//Setting up the stream
-
-Stream<QuerySnapshot> get User {
-    return Users.snapshots();
-}
 
 }
--- lib/pages/Patient/Selectdoctor.dart
+++ lib/pages/Patient/Selectdoctor.dart
@@ -1,4 +1,9 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:theroyalaesculapian/Connection/Database.dart';
+import 'package:theroyalaesculapian/Connection/Database.dart';
+import 'package:provider/provider.dart';
 
 class Selectdoctor extends StatefulWidget {
   const Selectdoctor({super.key});
@@ -30,151 +35,156 @@ var imgList = [
 class _SelectdoctorState extends State<Selectdoctor> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 3, 55, 57),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          title: Text(
-            "Select Doctor",
-            style: TextStyle(
-              color: Colors.white,
+    var streamProvider = StreamProvider<QuerySnapshot?>.value(
+      value: DatabaseService(uid: '').Doctor,
+      initialData: null,
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 3, 55, 57),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            title: Text(
+              "Select Doctor",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-        body: ListView.builder(
-            itemCount: imgList.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {},
-                child: Card(
-                  margin: EdgeInsets.only(
-                    top: 10,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        width: 91,
-                        height: 150,
-                        child: Image.asset(imgList[index]),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              docName[index],
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Container(
-                              width: 270,
-                              child: Text(
-                                descList[index],
-                                style:
-                                    TextStyle(fontSize: 15, color: Colors.grey),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Container(
-                              width: 200,
-                              child: Text(
-                                exp[index],
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.thumb_up_alt_sharp,
-                                  size: 20,
-                                  color: Colors.green,
-                                ),
-                                Text(
-                                  '95%',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 15,
-                                    color: Colors.green,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Icon(
-                                  Icons.message_sharp,
-                                  size: 20,
-                                  color: Colors.green,
-                                ),
-                                Text(
-                                  '30 Patient Stories',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 15,
-                                    color: Colors.green,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            ButtonBar(
-                              alignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                MaterialButton(
-                                  minWidth: 80,
-                                  onPressed: () {},
-                                  child: Expanded(
-                                    child: Text(
-                                      'Know More',
-                                    ),
-                                  ),
-                                ),
-                                MaterialButton(
-                                  minWidth: 160,
-                                  color: Color.fromARGB(255, 229, 233, 241),
-                                  onPressed: () {},
-                                  child: Expanded(
-                                    child: Text(
-                                      'Book Appointment',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+          body: ListView.builder(
+              itemCount: imgList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    margin: EdgeInsets.only(
+                      top: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: 91,
+                          height: 150,
+                          child: Image.asset(imgList[index]),
                         ),
-                      )
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                docName[index],
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 270,
+                                child: Text(
+                                  descList[index],
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.grey),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                width: 200,
+                                child: Text(
+                                  exp[index],
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.thumb_up_alt_sharp,
+                                    size: 20,
+                                    color: Colors.green,
+                                  ),
+                                  Text(
+                                    '95%',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 15,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(
+                                    Icons.message_sharp,
+                                    size: 20,
+                                    color: Colors.green,
+                                  ),
+                                  Text(
+                                    '30 Patient Stories',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 15,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              ButtonBar(
+                                alignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  MaterialButton(
+                                    minWidth: 80,
+                                    onPressed: () {},
+                                    child: Expanded(
+                                      child: Text(
+                                        'Know More',
+                                      ),
+                                    ),
+                                  ),
+                                  MaterialButton(
+                                    minWidth: 160,
+                                    color: Color.fromARGB(255, 229, 233, 241),
+                                    onPressed: () {},
+                                    child: Expanded(
+                                      child: Text(
+                                        'Book Appointment',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }),
+                );
+              }),
+        ),
       ),
     );
+    return streamProvider;
   }
 }
